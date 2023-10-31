cf6cfece8858b33df92a107f245e752bf5a6878a
dhanavishnu13 <vishnukvka@gmail.com>
2023-10-31 15:02:03 +0530

    first commit

---
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -0,0 +1,10 @@
+// Create two variables, firstName and lastName
+let firstName = 'Dhana'
+let lastName = 'Vishnu'
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + lastName
+// Log fullName to the console
+
+console.log(fullName)
+
+
